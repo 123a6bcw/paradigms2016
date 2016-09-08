98da25a3b3a3e3e7719688bb41f0c8a3098b6ca5
Александр Гладков <ga.karandash@yandex.ru>
2016-09-08 23:02:46 +0400

    Update wordcount.py

--- dzpy/1/wordcount.py
+++ dzpy/1/wordcount.py
@@ -41,7 +41,6 @@ def read_words(filename):
 def solve(filename):
     words = read_words(filename)
     words.sort()
-    
     ret = []
     i = 0
     while i < len(words):
@@ -51,7 +50,6 @@ def solve(filename):
             count += 1
         ret.append((words[i], count))
         i += 1
-    
     return ret    
 
 def second(pair):
